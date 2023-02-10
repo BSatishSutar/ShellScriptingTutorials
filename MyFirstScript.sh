3b432ccc3069d539a457cf70f215e4df28a0a916
Satish Sutar <sssatishsutar@gmail.com>
2023-02-10 12:44:13 +0530

    Adding First shell script files to local repo

---
+++ MyFirstScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this is my shell script
+
+echo "-------------------------------------------"
+echo "    Let's start learning Shell Scripting   "
+echo "    "
+echo "This is going to be an enriching journey"
+echo "-------------------------------------------"
