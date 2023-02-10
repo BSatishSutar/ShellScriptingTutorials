51498e9631f8656c7010c9eaf5f769738004dcef
Satish Sutar <sssatishsutar@gmail.com>
2023-02-10 19:52:48 +0530

    adding another shell script file

---
+++ leapyear.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter a year: "
+read year
+
+if [ $(($year % 4)) -eq 0 ]; then
+  if [ $(($year % 100)) -eq 0 ]; then
+    if [ $(($year % 400)) -eq 0 ]; then
+      echo "$year is a leap year."
+    else
+      echo "$year is not a leap year."
+    fi
+  else
+    echo "$year is a leap year."
+  fi
+else
+  echo "$year is not a leap year."
+fi
